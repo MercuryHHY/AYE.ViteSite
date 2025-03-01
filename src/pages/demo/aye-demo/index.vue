ac5d241c56fa48a2b136ad34db7b86f92a15a516
huanghuye <3294084766@qq.com>
2025-03-01 10:25:15 +0800

    chore:测试OK

--- src/pages/demo/aye-demo/index.vue
+++ src/pages/demo/aye-demo/index.vue
@@ -1,7 +1,23 @@
 <script lang="ts" setup>
+import { testAyeApi } from "@@/apis/ayedemos"
 
+defineOptions({
+  // 命名当前组件
+  name: "AyeDemo"
+})
+
+function test() {
+  testAyeApi().then((res: any) => {
+    console.log(res)
+    console.log(res.data)
+  })
+}
 </script>
 
 <template>
-  <P>你好，恭喜你，啊烨，这是新的开始</P>
+  <div class="app-container">
+    <el-button type="primary" @click="test">
+      测试
+    </el-button>
+  </div>
 </template>
